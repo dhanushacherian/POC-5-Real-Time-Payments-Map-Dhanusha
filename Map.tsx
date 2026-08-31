38abee8705cf48648ddfe2c639cb09934bb540d8
dhanushacherian <dhanushacherian@zohomail.in>
2026-08-31 23:07:14 +0530

    Update Map.tsx

--- Map.tsx
+++ Map.tsx
@@ -76,6 +76,7 @@ export default function PaymentMap({
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          noWrap={true}
         />
 
         {schemes.map((item) => {
